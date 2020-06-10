1fda807350aef8e61111e0537dcbecc916df2f86
Paola <pvillegasch@gmail.com>
2020-06-10 00:03:21 -0600

    Agregado array de Car

--- src/Client/Gui/PrincipalMenu.java
+++ src/Client/Gui/PrincipalMenu.java
@@ -1,5 +1,7 @@
 package Client.Gui;
 
+import Client.Logic.Player;
+import Client.Sprites.Car;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -83,29 +85,48 @@ public class PrincipalMenu extends Application {
 
         Text title = new Text("Seleccione un carro");
 
-        ImageView redCar = loadImageView("/res/redCar.png", 150, 150);
-        ImageView blueCar = loadImageView("/res/blueCar.png", 150, 150);
-        ImageView purpleCar = loadImageView("/res/purpleCar.png", 150, 150);
-        ImageView whiteCar = loadImageView("/res/whiteCar.png", 150, 150);
-        ImageView[] carArrayS = new ImageView[4];
-        carArrayS[0] = blueCar;
-        carArrayS[1] = purpleCar;
-        carArrayS[2] = redCar;
-        carArrayS[3] = whiteCar;
+//        ImageView redCar = loadImageView("/res/redCar.png", 150, 150);
+//        ImageView blueCar = loadImageView("/res/blueCar.png", 150, 150);
+//        ImageView purpleCar = loadImageView("/res/purpleCar.png", 150, 150);
+//        ImageView whiteCar = loadImageView("/res/whiteCar.png", 150, 150);
+//        ImageView[] carArrayS = new ImageView[4];
+//        carArrayS[0] = blueCar;
+//        carArrayS[1] = purpleCar;
+//        carArrayS[2] = redCar;
+//        carArrayS[3] = whiteCar;
         // TODO recibir array del sprite car e iterarlo para conocer el color del carro
+
+        Car[] arrayOfCars = new Car[4];
+        Car redCar = new Car("Rojo");
+        Car blueCar = new Car("Azul");
+        Car purpleCar = new Car("Morado");
+        Car whiteCar = new Car("Blanco");
+
+        redCar.setImage("/res/redCar.png", 150, 150);
+        blueCar.setImage("/res/blueCar.png", 150, 150);
+        purpleCar.setImage("/res/purpleCar.png", 150, 150);
+        whiteCar.setImage("/res/whiteCar.png", 150, 150);
+
+        arrayOfCars[0] = redCar;
+        arrayOfCars[1] = blueCar;
+        arrayOfCars[2] = purpleCar;
+        arrayOfCars[3] = whiteCar;
+
 
         for (int i = 0; i < 4; i++) {
             System.out.println("Agregando carro...");
-            ImageView tmp = carArrayS[i];
+            Car tmp = arrayOfCars[i];
+            ImageView tmpIV = loadImageView(tmp.getImage(), tmp.getHeight(), tmp.getWidth());
             DropShadow shadow = new DropShadow();
 
-            tmp.setOnMouseEntered(mouseEvent -> tmp.setEffect(shadow));
-            tmp.setOnMouseExited(mouseEvent -> tmp.setEffect(null));
-            tmp.setOnMouseClicked(mouseEvent -> {
-                System.out.println("Carro seleccionado");
+            tmpIV.setOnMouseEntered(mouseEvent -> tmpIV.setEffect(shadow));
+            tmpIV.setOnMouseExited(mouseEvent -> tmpIV.setEffect(null));
+            tmpIV.setOnMouseClicked(mouseEvent -> {
+                System.out.println("Carro " + tmp.getCarColor() + " seleccionado");
+                Player actualPlayer = new Player(tmp);
                 // TODO llamar a la ventana principal del juego
             });
-            carArray.getChildren().add(tmp);
+            carArray.getChildren().add(tmpIV);
         }
         title.getStyleClass().add("fancytext1");
         carMenuWindow.getChildren().addAll(title, carArray);
@@ -136,6 +157,15 @@ public class PrincipalMenu extends Application {
      */
     private ImageView loadImageView(String path, Integer height, Integer width){
         Image tokenImage = imageLoader(cwd.replaceAll("\\\\", "/") + path);
+        ImageView addTokenImage = new ImageView(tokenImage);
+        addTokenImage.setFitHeight(height);
+        addTokenImage.setFitWidth(width);
+
+        return addTokenImage;
+    }
+
+    private ImageView loadImageView(Image tokenImage, Integer height, Integer width){
+//        Image tokenImage = imageLoader(cwd.replaceAll("\\\\", "/") + path);
         ImageView addTokenImage = new ImageView(tokenImage);
         addTokenImage.setFitHeight(height);
         addTokenImage.setFitWidth(width);
