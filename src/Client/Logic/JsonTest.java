8409b18e68b9a4d394d6ee9ff2ede2867bd62054
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-12 16:59:30 -0600

    Prueba de la librería para manejar json

---
+++ src/Client/Logic/JsonTest.java
@@ -0,0 +1,24 @@
+package Client.Logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JsonTest {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        var json = "{\"status\":\"success\"}";
+        JsonNode root;
+        try {
+            root = mapper.readTree(json);
+        } catch (JsonProcessingException ex) {
+            System.err.println("Error parsing json.");
+            return;
+        }
+
+        System.out.println(root.toPrettyString());
+    }
+}
