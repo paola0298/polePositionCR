8eeeb9bf19f7d7f1956a61c3b5d666ef576cad50
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-13 21:23:03 -0600

    Se anula el método para generar movimiento aleatorio en altas velocidades.

--- src/Client/Sprites/Car.java
+++ src/Client/Sprites/Car.java
@@ -1,5 +1,9 @@
 package Client.Sprites;
 
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.Random;
+
 public class Car extends Sprite{
     private String carColor;
 
@@ -15,4 +19,15 @@ public class Car extends Sprite{
     public void setCarColor(String carColor) {
         this.carColor = carColor;
     }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        Random rand = new Random();
+        //Double offsetX = (rand.nextDouble() * getVelocityX()) / 60f;
+        Double offsetX = 0d;
+        Double offsetY = (rand.nextDouble() * getVelocityY()) / 40f;
+
+        gc.drawImage(getImage(), getPosX()  + offsetX, getPosY() + offsetY);
+    }
+
 }
