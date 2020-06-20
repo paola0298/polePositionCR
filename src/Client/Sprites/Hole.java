d2d3fac8c03892c0d38d9895ee2f6ba15f94016c
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-20 12:44:33 -0600

    Agregado método para indicar si el carro chocó con el hueco

--- src/Client/Sprites/Hole.java
+++ src/Client/Sprites/Hole.java
@@ -3,10 +3,27 @@ package Client.Sprites;
 /**
  * Clase del hueco
  */
-public class Hole {
+public class Hole extends Sprite{
+
+    public Integer id;
+    public Boolean carCrashed;
 
     /**
      * Constructor de la clase del hueco
      */
-    public Hole() { }
+    public Hole() {
+        carCrashed = false;
+    }
+
+    public void setCarCrashed(Boolean carCrashed) {
+        this.carCrashed = carCrashed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
