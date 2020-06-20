88f6a9ed272fcf8783f6dff8bf4760a29994e95b
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-20 14:30:43 -0600

    Añadida propiedad para indicar si se agarró el turbo

--- src/Client/Sprites/Turbo.java
+++ src/Client/Sprites/Turbo.java
@@ -6,11 +6,20 @@ package Client.Sprites;
 public class Turbo extends Sprite{
 
     public Integer id;
+    public Boolean turboGot;
 
     /**
      * Constructor de la clase Turbo
      */
     public Turbo () { }
+
+    public Boolean getTurboGot() {
+        return turboGot;
+    }
+
+    public void setTurboGot(Boolean turboGot) {
+        this.turboGot = turboGot;
+    }
 
     public Integer getId() {
         return id;
