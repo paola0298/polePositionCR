6d10ce4bc113e405277806c993cdf3c137858691
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-20 12:54:10 -0600

    Agregada propiedad Id

--- src/Client/Sprites/Turbo.java
+++ src/Client/Sprites/Turbo.java
@@ -5,8 +5,18 @@ package Client.Sprites;
  */
 public class Turbo extends Sprite{
 
+    public Integer id;
+
     /**
      * Constructor de la clase Turbo
      */
     public Turbo () { }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
